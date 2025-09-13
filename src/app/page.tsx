648701a1a35ebf2cb8ac5d2d5f030ef0c79f26d3
KrimsonJosh <jumoinzad@gmail.com>
2025-09-13 00:26:26 -0500

    add h1 for day 0

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Day 0</h1>
       <h1>im cooked bro</h1>
     </main>
   );
